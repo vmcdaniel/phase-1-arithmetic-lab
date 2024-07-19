5b088a7b847f8e834aac4f76d48eb57adaf8069b
Victoria <vmcdaniel459@gmail.com>
2024-07-18 20:30:14 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply;
+multiply = num1 * num2;
+
+let random;
+random = Math.floor(Math.random() * 10) + 2;
+
+let mod;
+let num3 = 4;
+let num4 = 16;
+mod = num3 % num4;
+
+let max = Math.max(5, 7,8, 20);
+
+
+
+
